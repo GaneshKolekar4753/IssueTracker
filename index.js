f6078c3a6e6254f639d45054c377a5def1e9d5cb
Ganesh Kolekar <kolekarganesh5151@gmail.com>
2023-12-16 15:30:08 +0530

    set view engine

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express=require('express');
 const port=7000;
 const app=express();
 
+const path=require('path');
+
 //use express router
 app.use('/',require('./routes/index'));
 
+//setup view engine(ejs)
+app.set('view engine','ejs');
+console.log(__dirname);
+app.set('views','./views');
 app.listen(port,function(err){
     if(err){
         console.log('Error in creating server',err);
